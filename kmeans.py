b2e00504a25f5760d94f6bd96ce09606b2e78085
Elmira Gebenova <elmira.gebenova@gmail.com>
2016-07-15 12:59:24 +0200

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,5 @@
 
- # K-Means model
+ # K-Means model:
  
  from sklearn.cluster import KMeans
  
@@ -20,3 +20,37 @@ n_jobs
 random_state
 
 '''
+
+
+# Graphing the variance:
+
+#determine the range of K-s
+k_range = range(1,14)
+
+# Fit the kmeans model for each n_clusters = k
+k_means_var = [KMeans(n_clusters=k).fit(hps) for k in k_range]
+
+#pull out the cluster centers for each model
+centroids = [X.cluster_centers_ for X in k_means_var]
+
+#Calculate the Euclidean distance from each point to each cluster center:
+
+k_euclid = [cdist(hpc, cent, 'euclidean') for cent in centroids]
+dist = [np.min(ke, axis = 1) for ke in k_euclid]
+
+
+#Total within-cluster sum of squares:
+wcss = [sum(d**2) for d in dist]
+
+# Total sum of squares:
+tss = sum(pdist(hpc)**2)/hpc.shape[0]
+
+#The between-cluster sum of squares:
+bss = tss - wcss
+
+
+
+
+
+
+
