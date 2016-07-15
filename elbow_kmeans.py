850a59a34134f89735a54371c7a5ac354df79ebd
Elmira Gebenova <elmira.gebenova@gmail.com>
2016-07-15 12:20:05 +0200

    Create elbow_kmeans.py
    shows the function of creating an 'elbow' graph to pick-up the optimal K for K-Means.

---
+++ elbow_kmeans.py
@@ -0,0 +1,20 @@
+
+# Elbow function for finding the optimal K in K-Means:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist, pdist
+import numpy as np
+
+def eblow(df, n):
+    kMeansVar = [KMeans(n_clusters=k).fit(df.values) for k in range(1, n)]
+    centroids = [X.cluster_centers_ for X in kMeansVar]
+    k_euclid = [cdist(df.values, cent) for cent in centroids]
+    dist = [np.min(ke, axis=1) for ke in k_euclid]
+    wcss = [sum(d**2) for d in dist]
+    tss = sum(pdist(df.values)**2)/df.values.shape[0]
+    bss = tss - wcss
+    plt.plot(bss)
+    plt.show()
